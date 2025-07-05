132cedc632f157c2521a65d4fa30cd7cfd9291ea
Anna Khodyreva <anoann@Annas-MacBook-Air.local>
2025-07-05 13:48:52 +0200

    Setup testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,5 @@
 
 int main()
 {
-
-	int clues[][16] = {
-	    { 2, 2, 1, 3,
-	    2, 2, 3, 1,
-	    1, 2, 2, 3,
-	    3, 2, 1, 3 }
-	};
-    int numClueSets = sizeof(clues) / sizeof(clues[0]);
-	
-    for (int i = 0; i < numClueSets; ++i)
-	{
-        int **b = Solver::SolvePuzzle(clues[i]);
-        Solver::PrintGrid(b);
-        Solver::DeleteGrid(b);
-    }
-
 	return 0;
 }
